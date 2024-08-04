dbe1c14470391b849362083e6a468daa26738384
Carlo Ramos <111720695+Zineey@users.noreply.github.com>
2024-08-04 14:04:32 +0800

    Log out confirmation

--- todo/src/components/Navbar.js
+++ todo/src/components/Navbar.js
@@ -1,16 +1,38 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
+
+const MySwal = withReactContent(Swal);
 const Navbar = () => {
 
     const {logout} = useLogout();
     const {user} = useAuthContext();
 
 
-    const handleLogout = () => {
-        logout();
-    }
+    const handleLogout = async () => {
+        const result = await MySwal.fire({
+            title: 'Are you sure?',
+            text: 'Do you want to log out?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, log out!',
+            cancelButtonText: 'Cancel'
+        });
+
+        if (result.isConfirmed) {
+            logout();
+            MySwal.fire(
+                'Logged out!',
+                'You have been logged out.',
+                'success'
+            );
+        }
+    };
 
     return (    
         <header>
